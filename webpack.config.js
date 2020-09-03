88f8197c0cacaeb90b3b09bd612e78663e23222f
Vitalii <vitalik77795@ukr.net>
2020-09-03 17:48:57 +0300

    Finish base framework funtional

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,8 @@ const jsLoaders = () => {
       loader: 'babel-loader',
       options: {
         presets: ['@babel/preset-env'],
+        plugins: ['@babel/plugin-syntax-class-properties',
+          'babel-plugin-transform-class-properties']
       },
     },
   ];
